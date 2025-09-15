aa20909e4010c8bbf669ad6849e8b73e26dd0955
Эмиров Нурлан Союнович <eminur@mail.ru>
2025-09-15 11:20:30 +0500

    реализация тестов

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -13,10 +13,16 @@ describe('PostsService', () => {
   });
 
   it('should add a new post', () => {
+    
+    
     // реализуйте тест-кейс
+    postsService.create(post);
+    expect(postsService.find('2')).toEqual(post); 
+
   });
 
   it('should find a post', () => {
     // реализуйте тест-кейс
+    expect(postsService.find('2')).toEqual(post); 
   });
 });
